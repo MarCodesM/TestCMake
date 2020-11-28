285e00d5fead464daf016a9822a39b8b04c9f4d2
MarCodesM <marcos.murillo395@my.tccd.edu>
2020-11-28 16:23:23 -0600

    Add project files.

---
+++ TestCMake/TestCMake.cpp
@@ -0,0 +1,22 @@
+﻿// TestCMake.cpp : Defines the entry point for the application.
+//
+
+#include "TestCMake.h"
+
+
+
+int main()
+{
+	std::vector<int> vec_i;
+	
+	for (int i = 0; i < 10; i++)
+	{
+		vec_i.push_back(i);
+	}
+
+	for (int val : vec_i)
+	{
+		std::cout << val << std::endl;
+	}
+	return 0;
+}
